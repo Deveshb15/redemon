d8e9120ce018ec6cea44b3544624fcdb48633e42
Deveshb15 <deveshb583@gmail.com>
2021-12-05 00:11:03 +0530

    fix: register username and password dummy validation

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -32,15 +32,33 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
-    @Mutation(() => User)
+    @Mutation(() => UserResponse)
     async register(
         @Arg("options") options: UsernamePasswordInput,
         @Ctx() ctx: MyContext
-    ) {
+    ) : Promise<UserResponse> {
+
+        if(options.username.length <= 2) {
+            return {
+                errors: [{
+                    field: "username",
+                    message: "username must be longer than 2 characters"
+                }]
+            }
+        }
+        if(options.password.length <= 3) {
+            return {
+                errors: [{
+                    field: "password",
+                    message: "password must be longer than 3 characters"
+                }]
+            }
+        }
+
         const hashedPassword = await argon2.hash(options.password)
         const user = ctx.em.create(User, { username: options.username, password: hashedPassword })
         await ctx.em.persistAndFlush(user)
-        return user;
+        return {user};
     }
 
     @Mutation(() => UserResponse)
