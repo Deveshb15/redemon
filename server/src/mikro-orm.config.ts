ad2895043ac84c49e649834eebff41712202f749
Deveshb15 <deveshb583@gmail.com>
2021-12-04 21:29:06 +0530

    changed directories

---
+++ server/src/mikro-orm.config.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv'
+import { Post } from "./entities/Post";
+import { __prod__ } from "./constants";
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+
+dotenv.config()
+
+export default {
+    migrations: {
+        path: path.join(__dirname, "./migrations"),
+        pattern: /^[\w-]+\d+\.[tj]s$/,
+    },
+    entities: [Post],
+    dbName: process.env.POSTGRES_DBNAME,
+    user: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    type: 'postgresql',
+    debug: !__prod__,
+} as Parameters<typeof MikroORM.init>[0]
