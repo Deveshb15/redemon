9659110448958a52bb1ed3f21cdc1e143cbea721
Deveshb15 <deveshb583@gmail.com>
2021-12-05 19:11:43 +0530

    sessions setyp

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,18 +10,45 @@ import { HelloResolver } from './resolvers/hello'
 import { PostResolver } from './resolvers/post'
 import { UserResolver } from "./resolvers/user"
 
+import Redis from 'ioredis'
+import session from 'express-session'
+import connectRedis from 'connect-redis'
+import { MyContext } from "./types"
+
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig)
     await orm.getMigrator().up()
 
     const app = express()
 
+    const RedisStore = connectRedis(session)
+    const redisClient = new Redis(14584, 'redis-14584.c264.ap-south-1-1.ec2.cloud.redislabs.com', {password: 'GqbU8YqB8blnJsOaEGit32oxDXOJDuFy'})
+
+    app.use(
+        session({
+            name: 'qid',
+            store: new RedisStore({ 
+                client: redisClient,
+                disableTouch: true
+            }),
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+                httpOnly: true,
+                sameSite: 'lax', // csrf
+                secure: __prod__,
+            },
+            saveUninitialized: false,
+            secret: 'deveshb',
+            resave: false,
+        })
+    )
+
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers: [HelloResolver, PostResolver, UserResolver],
             validate: false
         }),
-        context: () => ({ em: orm.em })
+        context: ({ req, res }): MyContext => ({ em: orm.em, req, res })
     })
     await apolloServer.start()
     apolloServer.applyMiddleware({ app })
